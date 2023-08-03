8d82012f81397805aa191c8dba66517a1a73cca0
VladZera <vladzevar@gmail.com>
2023-08-04 00:37:50 +0200

    fixed login page tests

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -4,6 +4,7 @@ from pages.login_page import LoginPage
 def test_log_in_error_when_incorrect_email(driver):
     login_page = LoginPage(driver)
     login_page.open_page()
+    login_page.cookies()
     driver.implicitly_wait(5)
     login_page.enter_email('aaaaaaaa.com')
     assert login_page.email_message_error.is_displayed()
@@ -12,6 +13,7 @@ def test_log_in_error_when_incorrect_email(driver):
 def test_log_in_error_when_short_password(driver):
     login_page = LoginPage(driver)
     login_page.open_page()
+    login_page.cookies()
     driver.implicitly_wait(5)
     login_page.enter_password('12345')
     assert login_page.password_message_error.is_displayed()
@@ -20,6 +22,7 @@ def test_log_in_error_when_short_password(driver):
 def test_login_button_is_visible_when_email_and_passwd_correct(driver):
     login_page = LoginPage(driver)
     login_page.open_page()
+    login_page.cookies()
     driver.implicitly_wait(5)
     login_page.enter_email('1234@gmail.com')
     login_page.enter_password('Aa12345678!')
@@ -29,6 +32,7 @@ def test_login_button_is_visible_when_email_and_passwd_correct(driver):
 def test_warning_account_not_exist(driver):
     login_page = LoginPage(driver)
     login_page.open_page()
+    login_page.cookies()
     login_page.enter_email('1234@gmail.com')
     login_page.enter_password('Aa12345678!')
     driver.implicitly_wait(5)
